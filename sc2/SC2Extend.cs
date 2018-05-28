9ba125b2572b478dbd5c5a07593223c4f6c39381
paweenwich <paweenwich@yahoo.com>
2018-05-28 15:56:57 +0700

    sdsd

---
+++ sc2/SC2Extend.cs
@@ -0,0 +1,31 @@
+﻿using SC2APIProtocol;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sc2
+{
+    public static class SC2Extend
+    {
+        public static Bitmap ToBitmap(this ImageData imgData)
+        {
+            return ByteArrayToBitmap(imgData.Data.ToByteArray(), imgData.Size.X, imgData.Size.Y);
+        }
+
+        public static Bitmap ByteArrayToBitmap(byte[] data, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+            Marshal.Copy(data, 0, bmpData.Scan0, data.Length);
+            bmp.UnlockBits(bmpData);
+            return bmp;
+        }
+
+    }
+
+}
